1e365c9124c2bb23ea0f7d4573b2ddf38ec6d505
CodewithShaaz <shaazahmed.ksa@gmail.com>
2025-04-23 00:06:50 +0530

    init

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/CodewithShaaz",
   resume:
-    "https://drive.google.com/file/d/1IToC2kVFml_YGxcfV26fWUbWr0MP7bao/view?usp=drive_link",
+    "https://drive.google.com/file/d/13f10lzAlEko0O055t7DG88-0EIisnZFJ/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/md-shaaz-ahmed-a078b2246/",
   twitter: "https://x.com/Shaaz1111",
   insta: "https://www.instagram.com/dark..knightt/",
